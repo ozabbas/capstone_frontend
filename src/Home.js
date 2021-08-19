b8fb87aadc4546933098de4cac1453289d03d4cb
Owais Abbas <ozabbas95@gmail.com>
2021-08-19 18:06:46 -0500

    attempted login page

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <>
-      <iframe src="https://giphy.com/embed/xUA7bfl3JCNxg14FYA" width="800" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/studiosoriginals-baseball-xUA7bfl3JCNxg14FYA"></a></p>
+      {/* <iframe src="https://giphy.com/embed/xUA7bfl3JCNxg14FYA" width="800" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/studiosoriginals-baseball-xUA7bfl3JCNxg14FYA"></a></p> */}
     </>
   )
 }
