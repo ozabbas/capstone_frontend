4b63d9078f241a603bbdb53ad7935ac80c99c737
Owais Abbas <ozabbas95@gmail.com>
2021-08-21 08:08:44 -0500

    login

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Home() {
   return (
     <>
       {email && <h1>You're logged in as {email}</h1>}
-      {/* <iframe src="https://giphy.com/embed/xUA7bfl3JCNxg14FYA" width="800" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/studiosoriginals-baseball-xUA7bfl3JCNxg14FYA"></a></p> */}
+
     </>
   )
 }
